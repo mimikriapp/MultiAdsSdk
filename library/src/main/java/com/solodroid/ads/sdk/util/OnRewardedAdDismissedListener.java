8e791b2a59229a6217ee16a91c1ccdbfda887903
mimikriapp <app.mimikri@gmail.com>
2023-08-30 14:54:08 +0700

    fix

--- library/src/main/java/com/solodroid/ads/sdk/util/OnRewardedAdDismissedListener.java
+++
@@ -1,5 +0,0 @@
-package com.solodroid.ads.sdk.util;
-
-public interface OnRewardedAdDismissedListener {
-    void onRewardedAdDismissed();
-}
--- library/src/main/java/com/solodroid/ads/sdk/util/OnRewardedAdErrorListener.java
+++
@@ -1,5 +0,0 @@
-package com.solodroid.ads.sdk.util;
-
-public interface OnRewardedAdErrorListener {
-    void onRewardedAdError();
-}
